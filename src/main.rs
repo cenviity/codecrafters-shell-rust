29eccd318ca2f49519fd957877d2531b1d89d6bb
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-12-08 12:48:14 +1100

    extract `cmd_*` functions

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,29 @@ fn main() -> io::Result<()> {
             let input = input.trim();
             let tokens = tokens(input);
             match tokens[..] {
-                ["exit", code] => {
-                    let code: i32 = code.parse().expect("exit code should be a valid i32 value");
-                    process::exit(code);
-                }
-                ["echo", ..] => println!("{}", tokens[1..].join(" ")),
-                ["type", command] => {
-                    if BUILTIN_COMMANDS.contains(&command) {
-                        println!("{command} is a shell builtin");
-                    } else {
-                        println!("{command}: not found");
-                    }
-                }
+                ["exit", code] => cmd_exit(code),
+                ["echo", ..] => cmd_echo(&tokens[1..]),
+                ["type", command] => cmd_type(command),
                 _ => println!("{input}: command not found"),
             }
         }
+    }
+}
+
+fn cmd_exit(code: &str) {
+    let code: i32 = code.parse().expect("exit code should be a valid i32 value");
+    process::exit(code);
+}
+
+fn cmd_echo(tokens: &[&str]) {
+    println!("{}", tokens.join(" "));
+}
+
+fn cmd_type(command: &str) {
+    if BUILTIN_COMMANDS.contains(&command) {
+        println!("{command} is a shell builtin");
+    } else {
+        println!("{command}: not found");
     }
 }
 
