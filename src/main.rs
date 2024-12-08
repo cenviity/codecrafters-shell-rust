a57976e311d0651a3fba9b5bc1134f9d451ea44c
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-12-08 18:47:14 +1100

    allow `type` command to take more than one argument

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() -> io::Result<()> {
             match tokens[..] {
                 ["exit", code] => cmd_exit(code),
                 ["echo", ..] => cmd_echo(&tokens[1..]),
-                ["type", command] => cmd_type(command),
+                ["type", ..] => cmd_type(&tokens[1..]),
                 _ => println!("{input}: command not found"),
             }
         }
@@ -35,20 +35,22 @@ fn cmd_echo(tokens: &[&str]) {
     println!("{}", tokens.join(" "));
 }
 
-fn cmd_type(command: &str) {
-    if BUILTIN_COMMANDS.contains(&command) {
-        println!("{command} is a shell builtin");
-    } else if let Ok(path_env) = std::env::var("PATH") {
-        let mut full_paths = path_env
-            .split(":")
-            .map(|path_dir| Path::new(path_dir).join(command));
-        if let Some(path) = full_paths.find(|path| path.is_file()) {
-            let path = path
-                .to_str()
-                .expect("full path to command should be valid Unicode");
-            println!("{command} is {path}");
-        } else {
-            println!("{command}: not found");
+fn cmd_type(commands: &[&str]) {
+    for command in commands {
+        if BUILTIN_COMMANDS.contains(command) {
+            println!("{command} is a shell builtin");
+        } else if let Ok(path_env) = std::env::var("PATH") {
+            let mut full_paths = path_env
+                .split(":")
+                .map(|path_dir| Path::new(path_dir).join(command));
+            if let Some(path) = full_paths.find(|path| path.is_file()) {
+                let path = path
+                    .to_str()
+                    .expect("full path to command should be valid Unicode");
+                println!("{command} is {path}");
+            } else {
+                println!("{command}: not found");
+            }
         }
     }
 }
