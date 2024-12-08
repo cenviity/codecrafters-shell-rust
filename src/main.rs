7ff9c85548fe12ed2cde8f788638b03f52048e2d
Vincent Ging Ho Yim <cenviity@gmail.com>
2024-12-08 11:48:51 +1100

    recognise `exit` command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() -> io::Result<()> {
     loop {
@@ -9,8 +10,15 @@ fn main() -> io::Result<()> {
         // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
+
         if stdin.read_line(&mut input).is_ok() {
-            println!("{}: command not found", input.trim());
+            let input = input.trim();
+            let mut input_words = input.split_whitespace();
+            if input_words.next() == Some("exit") {
+                process::exit(0);
+            } else {
+                println!("{input}: command not found");
+            }
         }
     }
 }
